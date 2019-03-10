0881543b88e55db729c56bfbe617f506b4adad37
namelos <namelos@outlook.com>
2019-03-11 00:40:23 +0800

    move frequent renter point to renter

--- Customer.cs
+++ Customer.cs
@@ -19,21 +19,15 @@ namespace refactoring
             string result = "Rental Record for " + Name + "\n";
             foreach (var each in _rentals)
             {
-                frequentRenterPoints += FrequentRenterPoints(each);
+                frequentRenterPoints += each.FrequentRenterPoints;
 
-                result += "\t" + each.Movie.Title + "\t" + each.GetCharge() + "\n";
-                totalAmount += each.GetCharge();
+                result += "\t" + each.Movie.Title + "\t" + each.Charge + "\n";
+                totalAmount += each.Charge;
             }
 
             result += "Amount owed is " + totalAmount + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points";
             return result;
-        }
-
-        private int FrequentRenterPoints(Rental each)
-        {
-            if (each.Movie.PriceCode == MovieType.NewRelease && each.DaysRented > 1) return 2;
-            return 1;
         }
 
         public string Name
--- Rental.cs
+++ Rental.cs
@@ -22,28 +22,33 @@ namespace refactoring
             get { return _movie; }
         }
 
-        public double GetCharge()
+        public double Charge
         {
-            double thisAmount = 0;
-
-            switch (Movie.PriceCode)
+            get
             {
-                case MovieType.Regular:
-                    thisAmount += 2;
-                    if (DaysRented > 2)
-                        thisAmount += (DaysRented - 2) * 1.5;
-                    break;
-                case MovieType.NewRelease:
-                    thisAmount += DaysRented * 3;
-                    break;
-                case MovieType.Childrens:
-                    thisAmount += 1.5;
-                    if (this.DaysRented > 3)
-                        thisAmount += (DaysRented - 3) * 1.5;
-                    break;
-            }
+                double thisAmount = 0;
 
-            return thisAmount;
+                switch (Movie.PriceCode)
+                {
+                    case MovieType.Regular:
+                        thisAmount += 2;
+                        if (DaysRented > 2)
+                            thisAmount += (DaysRented - 2) * 1.5;
+                        break;
+                    case MovieType.NewRelease:
+                        thisAmount += DaysRented * 3;
+                        break;
+                    case MovieType.Childrens:
+                        thisAmount += 1.5;
+                        if (this.DaysRented > 3)
+                            thisAmount += (DaysRented - 3) * 1.5;
+                        break;
+                }
+
+                return thisAmount;
+            }
         }
+
+        public int FrequentRenterPoints => Movie.PriceCode == MovieType.NewRelease && DaysRented > 1 ? 2 : 1;
     }
 }

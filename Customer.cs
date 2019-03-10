ba500f092d6213fe43f3bd9ac9c9fd731cc09802
namelos <namelos@outlook.com>
2019-03-11 00:56:57 +0800

    replace render loops with pipeline

--- Customer.cs
+++ Customer.cs
@@ -5,33 +5,25 @@ namespace refactoring
 {
     public class Customer
     {
-        private readonly string _name;
         private List<Rental> _rentals = new List<Rental>();
 
         public Customer(string name)
         {
-            _name = name;
+            Name = name;
         }
 
         public string Statement()
         {
-            string result = "Rental Record for " + Name + "\n";
-            foreach (var each in _rentals)
-            {
-                result += "\t" + each.Movie.Title + "\t" + each.Charge + "\n";
-            }
-
-            result += "Amount owed is " + GetTotalAmount() + "\n";
-            result += "You earned " + FrequentRenterPoints() + " frequent renter points";
+            string result = $"Rental Record for {Name}\n";
+            result += _rentals.Aggregate(string.Empty, (acc, r) => $"{acc}\t{r.Movie.Title}\t{r.Charge}\n");
+            result += $"Amount owed is {GetTotalAmount}\n";
+            result += $"You earned {FrequentRenterPoints} frequent renter points";
             return result;
         }
 
-        private int FrequentRenterPoints() => _rentals.Sum(rental => rental.FrequentRenterPoints);
-        private double GetTotalAmount() => _rentals.Sum(rental => rental.Charge);
+        private int FrequentRenterPoints => _rentals.Sum(rental => rental.FrequentRenterPoints);
+        private double GetTotalAmount => _rentals.Sum(rental => rental.Charge);
 
-        public string Name
-        {
-            get { return _name; }
-        }
+        public string Name { get; }
     }
 }
